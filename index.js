ccc5f2739dba036d7a0b8f8035fde90d94a31546
Samuel Razali <samuelrazali@gmail.com>
2021-06-26 15:35:14 +1000

    refactor routes to be on its own file: routes.js

--- index.js
+++ index.js
@@ -1,48 +1,7 @@
 const http = require('http')
-const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-    const { url, method } = req
-    if (url === "/") {
-        res.write(`
-            <html>
-                <body>
-                    <h1>hello</h1>
-                    <form action="/message" method="POST">
-                        <input type="text" name="message"/>
-                        <button type"submit">Submit</button>
-                    </form
-                </body>
-            </html>
-        `)
-        return res.end()
-    }
+const reqHandler = require('./routes')
 
-    if (url === '/message' && method === 'POST') {
+const server = http.createServer(reqHandler)
 
-        // parsing request bodies
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log('chunk', chunk)
-            body.push(chunk)
-            console.log('body', body)
-        })
-
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const mes = parsedBody.split('=')[1].replace(/[+]/g, " ")
-            console.log("mes", mes)
-            fs.writeFile('message.txt', mes, () => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/')
-                return res.end();
-
-            })
-        })
-
-
-    }
-})
-
-console.log('Server ON')
 server.listen('3000')
---
+++ routes.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+
+const reqHandler = (req, res) => {
+    const { url, method } = req
+    if (url === "/") {
+        res.write(`
+            <html>
+                <body>
+                    <h1>hello</h1>
+                    <form action="/message" method="POST">
+                        <input type="text" name="message"/>
+                        <button type"submit">Submit</button>
+                    </form>
+                </body>
+            </html>
+        `)
+        return res.end()
+    }
+
+    if (url === '/message' && method === 'POST') {
+
+        // parsing request bodies
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log('chunk', chunk)
+            body.push(chunk)
+            console.log('body', body)
+        })
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const mes = parsedBody.split('=')[1].replace(/[+]/g, " ")
+            console.log("mes", mes)
+            fs.writeFile('message.txt', mes, () => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/')
+                return res.end();
+            })
+        })
+
+
+    }
+}
+
+module.exports = reqHandler
