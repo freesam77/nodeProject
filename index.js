a90064241539685b8b01e7fb65921cfa13862dde
Samuel Razali <samuelrazali@gmail.com>
2021-07-02 21:27:49 +1000

    add express.urlencoded instead of installing body-parser, handle post request and console body req on /users

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express()
+app.use(express.urlencoded({ extended: false }))
 
-app.use('/', (req, res, next) => {
-    console.log("I am groot!")
-    next()
+
+app.post('/users', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
 })
 app.use(['/hallo', '/hello'], (req, res, next) => {
     res.send('<h1>hallo</h1>')
+    next()
+})
+
+app.use('/', (req, res, next) => {
+    const form = `
+    <h1>Please type your name and click submit</h1>
+        <form method="POST" action="/users">
+            <input type="text" name="title"/>
+            <button type="submit">Submit</button>
+        </form>
+    `
+    res.send(form)
     next()
 })
 
