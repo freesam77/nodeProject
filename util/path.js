00f0b7bd36c984dccf36e7dd7d53e9788c30f8c2
Samuel Razali <srazali@atlassian.com>
2021-07-12 20:51:26 +1000

    nit refactor ternary operator on path.js

--- util/path.js
+++ util/path.js
@@ -1,3 +1,3 @@
 const path = require('path')
-const dir = require.main === null ? process.mainModule.filename : require.main.filename
+const dir = require.main ? require.main.filename : process.mainModule.filename 
 module.exports = path.dirname(dir)
