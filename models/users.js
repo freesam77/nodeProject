294310b67da59c49db85af423cbd2efaec60b51c
Samuel Razali <srazali@atlassian.com>
2021-07-28 10:46:09 +1000

    refactor users data and error message display when there is no users

--- models/users.js
+++ models/users.js
@@ -1,4 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const mainDir = require('../util/path');
 const users = [];
+
+const mainpath = path.join(mainDir, 'data', 'users.json');
 module.exports = class Users {
   constructor(name) {
     this.name = name;
@@ -6,9 +11,31 @@ module.exports = class Users {
 
   saveUsers() {
     users.push(this);
+    fs.readFile(mainpath, (err, fileContent) => {
+      let users = [];
+      if (!err) {
+        users = JSON.parse(fileContent);
+      }
+      users.push(this);
+      users = JSON.stringify(users);
+      fs.writeFile(mainpath, users, (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static getUsers() {
-    return users;
+    fs.readFile(mainpath, (err, fileContent) => {
+      if (err) {
+        console.log(err);
+        return [];
+      }
+      const data = JSON.parse(fileContent);
+      const newUsers = [];
+      data.map(({ name }) => {
+        newUsers.push(name);
+      });
+      return newUsers;
+    });
   }
 };
