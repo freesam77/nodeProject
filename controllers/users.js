6fa5bb549915480f8361456f2fe12bfcc7f555f8
Samuel Razali <srazali@atlassian.com>
2021-08-04 21:05:47 +1000

    add getUserId static function to model, change form style and user table render

--- controllers/users.js
+++ controllers/users.js
@@ -1,30 +1,33 @@
 const Users = require('../models/users');
 
 exports.getUsers = (req, res, next) => {
-    Users.getUsers((users) => {
-        res.render('users', {
-            users,
-            path: `/users`,
-            activeUser: true,
-            title: 'Users',
-        });
+  Users.getUsers((users) => {
+    res.render('users', {
+      users,
+      path: `/users`,
+      activeUser: true,
+      title: 'Users',
     });
+  });
 };
 
 exports.getUser = (req, res, next) => {
-    Users.getUsers((users) => {
-        res.render('users', {
-            users,
-            path: `/users${req.url}`,
-            activeUser: true,
-            title: 'Users',
-        });
+  Users.getUserId(req.params.userid, (user) => {
+    console.log(user);
+  });
+  Users.getUsers((users) => {
+    res.render('users', {
+      users,
+      path: `/users${req.url}`,
+      activeUser: true,
+      title: 'Users',
     });
+  });
 };
 
 exports.postUsers = (req, res, _next) => {
-    const users = new Users(req.body.name);
-    users.saveUser();
-    res.redirect('back');
-    return res.end();
+  const users = new Users(req.body);
+  users.saveUser();
+  res.redirect('back');
+  return res.end();
 };
--- models/users.js
+++ models/users.js
@@ -6,35 +6,44 @@ const { v4: uuidv4 } = require('uuid');
 const mainpath = path.join(mainDir, 'data', 'users.json');
 
 const readUsersFromFile = (cb) => {
-    fs.readFile(mainpath, (err, fileContent) => {
-        if (err) {
-            cb([]);
-        }
-        let usersList = JSON.parse(fileContent);
-        usersList.map((user) => {
-            if (user.id === undefined) {
-                user.id = uuidv4();
-            }
-        });
-        cb(usersList);
+  fs.readFile(mainpath, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    }
+    let usersList = JSON.parse(fileContent);
+    usersList.map((user) => {
+      if (user.id === undefined) {
+        user.id = uuidv4();
+      }
     });
+    cb(usersList);
+  });
 };
 module.exports = class Users {
-    constructor(name) {
-        this.name = name;
+  constructor(dataObj) {
+    for (const [key, value] of Object.entries(dataObj)) {
+      this[`${key}`] = value;
     }
+  }
 
-    saveUser() {
-        readUsersFromFile((userList) => {
-            this.id = uuidv4();
-            userList.push(this);
-            fs.writeFile(mainpath, JSON.stringify(userList), (err) => {
-                console.log(err);
-            });
-        });
-    }
+  saveUser() {
+    readUsersFromFile((userList) => {
+      this.id = uuidv4();
+      userList.push(this);
+      fs.writeFile(mainpath, JSON.stringify(userList), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-    static getUsers(cb) {
-        readUsersFromFile(cb);
-    }
+  static getUsers(cb) {
+    readUsersFromFile(cb);
+  }
+
+  static getUserId(id, cb) {
+    readUsersFromFile((users) => {
+      const user = users.find((u) => u.id === id);
+      cb(user);
+    });
+  }
 };
