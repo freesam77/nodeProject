03d5beff078b16a9619921e3b2ca235471d7920d
Samuel Razali <samuelrazali@gmail.com>
2021-08-14 11:53:23 +1000

    refactor form to its own ejs, add edit state to render form on user detail and style active for the edit button

--- controllers/users.js
+++ controllers/users.js
@@ -18,11 +18,14 @@ exports.getUsers = (req, res, next) => {
 
 exports.getUser = (req, res, next) => {
     Users.getUserId(req.params.userid, (user) => {
+        const { edit } = req.query
+        const booleanEdit = edit === 'true' ? true : false
         res.render('user-detail', {
             user,
             path: `/users${req.url}`,
             activeUser: true,
             title: 'Users',
+            edit: booleanEdit
         });
     });
 };
