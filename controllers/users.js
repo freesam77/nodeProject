56188cf0e7b6fa0c28d472a38678dcd033b05a0c
Samuel Razali <samuelrazali@gmail.com>
2021-10-17 18:11:48 +1100

    fix user detail page, edit user and delete user

--- controllers/users.js
+++ controllers/users.js
@@ -16,32 +16,48 @@ exports.getUsers = (_req, res, _next) => {
     });
 };
 
-exports.getUser = async (req, res, _next) => {
-    const user = await Users.getUserById(req.params.userid);
-    const edit = (query) => {
-        const { edit } = query;
-        return edit === 'true' ? true : false;
-    };
+exports.getUser = (req, res, _next) => {
+    Users.findById(req.params.userid, (err, user) => {
+        if (err) {
+            console.log(err)
+        }
 
-    res.render('user-detail', {
-        user,
-        path: `/users${req.url}`,
-        activeUser: true,
-        title: 'Users',
-        edit: edit(req.query),
+        const edit = (query) => {
+            const { edit } = query;
+            return edit === 'true' ? true : false;
+        };
+
+        res.render('user-detail', {
+            user,
+            path: `/users${req.url}`,
+            activeUser: true,
+            title: 'Users',
+            edit: edit(req.query),
+        });
     });
+
 };
 
 exports.postUsers = (req, res, _next) => {
-    const users = new Users({ ...req.body });
-    users.save()
+    const { userid } = req.params;
+    if (userid) {
+        Users.findByIdAndUpdate(userid, { ...req.body }, (err, docs) => {
+            console.log(err ? err : `Updated User: ${docs}`)
+        })
+    } else {
+
+        const users = new Users({ ...req.body });
+        users.save()
+    }
     res.redirect('back');
     return res.end();
 };
 
 exports.deleteUserById = (req, res, _next) => {
     const { userid } = req.params;
-    Users.deleteUserById(userid);
+    Users.findByIdAndDelete(userid, (err, docs) => {
+        console.log(err ? err : `Deleted User: ${docs}`)
+    })
     res.redirect('/users');
     return res.end();
 };
