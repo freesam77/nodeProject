91cb2d9a5ac0c468833bf74ddc76e4f02d6d0f8e
Samuel Razali <samuelrazali@gmail.com>
2021-12-11 20:58:48 +1100

    using mongodb to store sessions

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const mongoose = require('mongoose')
+const MongoDBStore = require('connect-mongodb-session')(session)
 const usersRoutes = require('./routes/users');
 const indexRoutes = require('./routes/index');
 const authRoutes = require('./routes/auth');
 const mailingListRoutes = require('./routes/mailing-list');
 const rootDir = require('./util/path');
 const { get404 } = require('./controllers/error')
-const app = express();
+
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
+const MONGODB_URI = `mongodb+srv://sam:${process.env.PASS}@cluster0.zuvwc.mongodb.net/myFirstDatabase?w=majority`
+
+const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -22,7 +30,8 @@ app.use(express.static(path.join(rootDir, 'public')));
 const sesh = {
     secret: 'my secret',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store
 }
 app.use(session(sesh))
 
@@ -34,7 +43,7 @@ app.use(authRoutes);
 // handle 404 error page
 app.use(get404);
 
-mongoose.connect(`mongodb+srv://sam:${process.env.PASS}@cluster0.zuvwc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
-    .then(res => {
+mongoose.connect(MONGODB_URI)
+    .then((_res) => {
         app.listen(3000)
     }).catch(err => console.log(err))
